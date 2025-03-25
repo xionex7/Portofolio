aa0acd4594d2b13b593e2219cfdb94e4eb2db02e
leonlelion3@gmail.com <leonlelion3@gmail.com>
2025-03-25 08:19:17 +0100

    feat: create the interface of the app todo list simple

--- todo-list-simple/src/app/app.component.ts
+++ todo-list-simple/src/app/app.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    FormsModule,
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-list-simple';
+  title = 'Simple Todo List';
+  newTask: string = '';
+  tasks: string[] = [];
+  isChecked: boolean = false;
+
+  addTask() {
+    this.tasks.push(this.newTask);
+  }
+
+  deleteTask(){
+    this.tasks.pop();
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+  saveTask(){
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+  checkValue(checked: boolean){
+    this.isChecked = checked;
+    if(this.isChecked == true){
+      this.saveTask();
+      this.isChecked = false;
+    }
+    else{
+      localStorage.removeItem('tasks');
+    }
+  }
 }
